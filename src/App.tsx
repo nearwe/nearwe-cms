fb47737426a392f8af3b7e38e58f2a374e708bad
piyush2053 <piyushpatel2053@gmail.com>
2026-05-01 03:08:03 +0530

    F3

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import { Layout } from "antd";
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
 import { NotificationProvider } from "./contexts/NotificationContext";
-import '../src/utils/css/custom.css'
+import "../src/utils/css/custom.css";
 import { UserProvider } from "./contexts/UserContext";
+
+// WEBSITE PAGES
 import NearWeLandingPage from "./pages/LandingPage";
 import AboutUs from "./pages/AboutUs";
+import ServicesPage from "./pages/Services";
+import ServicesAgencyPage from "./pages/CompanyServices";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import AdvertiseWithUs from "./pages/Advertise";
+import Contact from "./pages/Contact";
+import TermsAndConditions from "./pages/Terms";
+import ChildSafetyPolicy from "./pages/Child";
+
+// CMS PAGES
 import CmsHome from "./cms/cms_home";
 import UserManagement from "./cms/pages/UserManagement";
 import CategoryManagement from "./cms/pages/CategoryManagement";
@@ -16,17 +26,13 @@ import EventManagement from "./cms/pages/EventManagement";
 import AppManagement from "./cms/pages/AppManagement";
 import InsightsDashboard from "./cms/pages/InsightsDashboard";
 import Announcements from "./cms/pages/Announcements";
-import ChildSafetyPolicy from "./pages/Child";
-import TermsAndConditions from "./pages/Terms";
-import ServicesPage from "./pages/Services";
-import ServicesAgencyPage from "./pages/CompanyServices";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import AdvertiseWithUs from "./pages/Advertise";
-import Contact from "./pages/Contact";
 import Reports from "./cms/pages/Reports";
 import UserInterestDetail from "./cms/pages/Interest";
 
 const App = () => {
+  // 🔥 Detect subdomain
+  const isCMS = window.location.hostname.includes("cms");
+
   return (
     <AuthProvider>
       <UserProvider>
@@ -35,42 +41,48 @@ const App = () => {
             <Layout className="min-h-screen bg-bg1">
               <Layout.Content>
                 <Routes>
-                  <Route
-                    path="/cms"
-                    element={
-                      <ProtectedRoute>
-                        <CmsHome />
-                      </ProtectedRoute>
-                    }
-                  >
-                    <Route index element={<Navigate to="/cms/insights" replace />} />
 
-                    {/* INSIGHTS */}
-                    <Route path="insights" element={<InsightsDashboard />} />
+                  {/* ================= CMS ROUTES ================= */}
+                  {isCMS ? (
+                    <>
+                      <Route
+                        path="/"
+                        element={
+                          <ProtectedRoute>
+                            <CmsHome />
+                          </ProtectedRoute>
+                        }
+                      >
+                        <Route index element={<Navigate to="/insights" replace />} />
 
-                    {/* EXISTING */}
-                    <Route path="users" element={<UserManagement />} />
-                    <Route path="categories" element={<CategoryManagement />} />
-                    <Route path="events" element={<EventManagement />} />
-                    <Route path="app" element={<AppManagement />} />
-                    <Route path="announcements" element={<Announcements />} />
-                    <Route path="reports" element={<Reports />} />
-                    <Route path="interests" element={<UserInterestDetail />} />
-                  </Route>
-                  <Route path="/aboutus" element={<AboutUs />} />
-                  <Route path="/services" element={<ServicesPage />} />
-                  <Route path="/companyservices" element={<ServicesAgencyPage />} />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="/terms" element={<TermsAndConditions />} />
-                  <Route path="/advertise" element={<AdvertiseWithUs />} />
-                  <Route path="/privacy" element={<PrivacyPolicy />} />
-                  <Route path="/child-safety" element={<ChildSafetyPolicy />} />
-                  <Route path="/cms/login" element={<Login />} />
-                  <Route
-                    path="/"
-                    element={<NearWeLandingPage />}
-                  />
-                  
+                        <Route path="insights" element={<InsightsDashboard />} />
+                        <Route path="users" element={<UserManagement />} />
+                        <Route path="categories" element={<CategoryManagement />} />
+                        <Route path="events" element={<EventManagement />} />
+                        <Route path="app" element={<AppManagement />} />
+                        <Route path="announcements" element={<Announcements />} />
+                        <Route path="reports" element={<Reports />} />
+                        <Route path="interests" element={<UserInterestDetail />} />
+                      </Route>
+
+                      {/* LOGIN */}
+                      <Route path="/login" element={<Login />} />
+                    </>
+                  ) : (
+                    /* ================= WEBSITE ROUTES ================= */
+                    <>
+                      <Route path="/" element={<NearWeLandingPage />} />
+                      <Route path="/aboutus" element={<AboutUs />} />
+                      <Route path="/services" element={<ServicesPage />} />
+                      <Route path="/companyservices" element={<ServicesAgencyPage />} />
+                      <Route path="/contact" element={<Contact />} />
+                      <Route path="/terms" element={<TermsAndConditions />} />
+                      <Route path="/advertise" element={<AdvertiseWithUs />} />
+                      <Route path="/privacy" element={<PrivacyPolicy />} />
+                      <Route path="/child-safety" element={<ChildSafetyPolicy />} />
+                    </>
+                  )}
+
                 </Routes>
               </Layout.Content>
             </Layout>
